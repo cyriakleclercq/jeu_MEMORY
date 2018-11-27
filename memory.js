848e52f25c015d8549b7c422875af4034bc37252
cyriakleclercq <cyriakleclercq@gmail.com>
2018-11-27 11:52:37 +0100

    commit 3

--- memory.js
+++ memory.js
@@ -1,7 +1,7 @@
 
 
 
-
+/*
 //            ------------decompte function---------------a
 
 // innit variable
@@ -29,15 +29,8 @@ setTimeout(decompte, 1000);
 
 // init  table
 
-var img1 = document.getElementById("img1");
-var img2 = document.getElementById("img2");
-var img3 = document.getElementById("img3");
-var img4 = document.getElementById("img4");
-var img5 = document.getElementById("img5");
-var img6 = document.getElementById("img6");
+var tableau1 = [0, 1, 2, 3, 4, 5];
 
-
-var tableau1 = [img1, img2, img3, img4, img5, img6];
 
 var tableau2 = [];
 
@@ -48,6 +41,7 @@ var calc = 0;
 var choix_ordi = 0;
 
 function alea () {
+
     calc = Math.floor(Math.random() * tableau1.length);
 
     choix_ordi = tableau1[calc];
@@ -56,25 +50,137 @@ function alea () {
 
 while (tableau1.length) {
 
-alea();
+    var i = 0;
+    i ++;
 
-tableau2.push(choix_ordi);
 
-tableau1.splice(calc,1);
 
-console.log(tableau1);
-console.log(tableau2);
+    var im = document.createElement("img");
+    im.id = "im" + tableau2[i];
+
+
+
+
+    alea();
+
+    tableau2.push(choix_ordi);
+
+    tableau1.splice(calc,1);
+
+    img[i].appendChild(im);
+
+    console.log(tableau1);
+    console.log(tableau2);
+
+
+}
+*/
+
+var recto_carte = [0,0,1,1,2,2];
+
+var tableau1 = [0,0,0,0,0,0 ];
+
+var choix = [];
+
+var bonne_rep = 0;
+
+var imgCartes = document.getElementById("section1G").getElementsByTagName("img");
+
+for (i = 0; i < imgCartes.length; i++){
+
+    imgCartes[i].noCarte=i;
+
+    imgCartes[i].onclick=function(){
+
+        controleJeu(this.noCarte);
+    }
+}
+
+initialiseJeu();
+
+function majAffichage(noCarte) {
+
+
+    switch (tableau1[noCarte]) {
+        case 0:
+            imgCartes[noCarte].src = "dos_de_carte.png";
+            break;
+        case 1:
+            imgCartes[noCarte].src = "carte" + recto_carte[noCarte] + ".png";
+            break;
+        case -1:
+            imgCartes[noCarte].style.visibility = "hidden";
+            break;
+    }
+}
+
+function rejouer(){
+
+    document.getElementById("victoire").innerHTML = " WELL PLAYED";
+
+    location.reload();
 
 }
 
-var ab = document.getElementById("img1");
+function initialiseJeu(){
 
-var img1 = document.createElement("img");
-ab.appendChild(img1);
+    for (var position = recto_carte.length - 1; position >= 1; position-- ){
 
-img1.src = "OZHk.png";
-img1.style.width = "15%";
+        var hasard = Math.floor(Math.random()*(position+1));
 
+        var sauve = recto_carte[position];
+
+        recto_carte[position] = recto_carte[hasard];
+
+        recto_carte[hasard] = sauve;
+    }
+}
+
+function controleJeu(noCarte){
+
+    if(choix.length<2){
+
+        if(tableau1[noCarte]==0){
+
+            tableau1[noCarte]=1;
+
+            choix.push(noCarte);
+
+            majAffichage(noCarte);
+        }
+
+        if(choix.length==2){
+
+            var nouveauEtat = 0;
+
+            if(recto_carte[choix[0]] == recto_carte[choix[1]]){
+
+                nouveauEtat=-1;
+
+                bonne_rep ++;
+            }
+
+            tableau1[choix[0]] = nouveauEtat;
+
+            tableau1[choix[1]] = nouveauEtat;
+
+            setTimeout(function(){
+
+                majAffichage(choix[0]);
+
+                majAffichage(choix[1]);
+
+                choix=[];
+
+                if(bonne_rep == 3){
+
+                    rejouer();
+
+                }
+            },1000);
+        }
+    }
+}
 
 
 
