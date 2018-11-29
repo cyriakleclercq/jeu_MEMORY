f321ebb074111388bdb406977424e86fcb62a195
cyriakleclercq <cyriakleclercq@gmail.com>
2018-11-29 11:16:01 +0100

    commit 9 (test unitaire à faire)

--- memory.js
+++ memory.js
@@ -1,14 +1,15 @@
 
 
-var t_test = ["0","1","2","3","4","5"];
+// initializes the table
 
 var tableau1 = ["0", "0", "1", "1", "2", "2"];
 
+// table to find the right position on the card clicked
+var t_test = ["0","1","2","3","4","5"];
 
 var tableau2 = [];
 
-
-// function random
+// random function
 
 var calc = 0;
 var choix_ordi = 0;
@@ -20,7 +21,7 @@ function alea () {
     choix_ordi = tableau1[calc];
 }
 
-// melange le tableau
+// mix values of "tableau1" in "tableau2" and clean "tableau1"
 
 while (tableau1.length) {
 
@@ -33,15 +34,14 @@ while (tableau1.length) {
 
     tableau1.splice(calc,1);
 
-
-
 }
 
 console.log(tableau2);
 
-
+// table to compare the user choice
 var tableau3 = [];
 
+// table to point the card clicked by the user
 var tableau4 = [];
 
 var test = 0;
@@ -50,20 +50,25 @@ var limite = 0;
 
 var nbr_paire = 0;
 
+// display only "page1"
+
 document.getElementById("page1").style.display = "block";
 document.getElementById("page2").style.display = "none";
 
 
-// mise en place des event //
+// setting up the event  //
 
-// limite le nombre de carte selectionné à 2
 
 for (let i = 0; i < tableau2.length; i++) {
 
     document.getElementById("span" + i ).addEventListener("click", function() {
 
-            if (limite < 2) {
+        // limit the number of selected cards to 2
+
+        if (limite < 2) {
                 limite++;
+
+                // randomly put the new cards in the game
 
                 let spani = document.getElementById("span" + i);
                 spani.setAttribute("src", "carte" + tableau2[i] + ".png");
@@ -76,6 +81,10 @@ for (let i = 0; i < tableau2.length; i++) {
 
         setTimeout (function() {
 
+            // compare the value of "tableau3" //
+
+            // if value of "tableau3" is equal
+
             if (tableau3[0] == tableau3[1] && tableau3.length == 2  && tableau4[0] != tableau4[1]) {
 
                 nbr_paire ++;
@@ -84,6 +93,8 @@ for (let i = 0; i < tableau2.length; i++) {
                 tableau3 = [];
             }
 
+            // if value of "tableau3" isn't equal
+
             else if (tableau3[0] != tableau3[1] && tableau3.length == 2 && tableau4[0] != tableau4[1]) {
 
                 bad_answer();
@@ -91,13 +102,23 @@ for (let i = 0; i < tableau2.length; i++) {
                 tableau3 = [];
 
             }
+
+            // timer to display the card
+
         }, 2000);
+
+        // condition to win
 
         if (nbr_paire == 3){
 
+            //display the result
             document.getElementById("nbr_paire").innerHTML = nbr_paire;
+
+            // display only "page2"
             document.getElementById("page1").style.display = "none";
             document.getElementById("page2").style.display = "block";
+
+            // display a victory message
             document.getElementById("resultat").innerHTML = " WELL PLAYED";
 
         }
@@ -109,6 +130,7 @@ function good_answer() {
 
     for (let i = 0; i < tableau4.length; i++) {
 
+        //hide the card
         document.getElementById("span" + tableau4[i]).style.visibility = "hidden";
     }
 
@@ -120,6 +142,7 @@ function bad_answer() {
 
     for (let i = 0; i < tableau4.length; i++) {
 
+        //return the card
         document.getElementById("span" + tableau4[i]).src = "dos_de_carte.png";
     }
 
@@ -131,37 +154,50 @@ function bad_answer() {
 var s = 15;
 var clear;
 
+// countdown function
+
 function decompte() {
 
     clearTimeout(clear);
 
+    //display the countdown on the site
     document.getElementById("time").innerHTML = "Timer" + " " + ":" + " " + s--;
 
     clear = setTimeout(decompte, 1000);
 
-
+// if timer is down
     if (s < 0) {
 
         clearTimeout(clear);
 
+        //display only "page2"
         document.getElementById("page1").style.display = "none";
         document.getElementById("page2").style.display = "block";
 
+        //display the defeat message + number of correct choice
         document.getElementById("resultat").innerHTML = "YOU LOSE";
         document.getElementById("nbr_paire").innerHTML = nbr_paire;
 
+        // delay to retry
         setTimeout(rejouer, 3000);
 
     }
 
+    // if the user win
     if(nbr_paire == 3){
 
+        //stop the timer
         clearTimeout(clear);
 
+        //display only "page2"
         document.getElementById("page1").style.display = "none";
         document.getElementById("page2").style.display = "block";
+
+        //display the number of correct answer
         document.getElementById("nbr_paire").innerHTML = nbr_paire;
         document.getElementById("resultat").innerHTML = " WELL PLAYED";
+
+        // delay the retry
         setTimeout(rejouer, 3000);
 
     }
@@ -169,6 +205,7 @@ function decompte() {
 
 decompte();
 
+//replay function
 
 function rejouer(){
 
